9475ea5f5e46611ae1f1f5b8e82eddc1429dc4ca
Jeff <jmartinbaldwin@gmail.com>
2024-04-19 15:04:34 +0900

    Write the code to play one round of RPS and return result in a string

--- RPS.js
+++ RPS.js
@@ -24,6 +24,28 @@ let computerSelection = getComputerChoice();
 
 let playerSelection = getPlayerChoice();
 
-function playRound(playerSelection, computerSelection) {
-    if playerSelection === ""
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === "rock" && computerSelection === "rock"){
+        return "rock and rock: it's a tie."
+    } else if (playerSelection === "rock" && computerSelection === "paper"){
+        return "rock and paper: you lose."
+    } else if (playerSelection === "rock" && computerSelection === "scissors"){
+        return "rock and scissors: you win!"
+    } else if (playerSelection === "paper" && computerSelection === "paper"){
+        return "paper and paper: it's a tie."
+    } else if (playerSelection === "paper" && computerSelection === "scissor"){
+        return "paper and scissors: you lose."
+    } else if (playerSelection === "scissors" && computerSelection === "scissors"){
+        return "scissors and scissors: it's a tie."
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "scissors and rock: you lose!"
+    } else if (playerSelection === "scissors" && computerSelection === "paper"){
+        return "scissors and paper: you win!"
+    } else if (playerSelection === "paper" && computerSelection === "rock"){
+        return "paper and rock: you win!"
+    } else {
+        return "You must have typed something incorrectly. Please try again."
+    }
 }
+
+console.log(playRound(playerSelection, computerSelection));
