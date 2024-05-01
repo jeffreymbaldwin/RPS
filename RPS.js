23a42253e25569baca55129ab8a93ad0430940b0
Jeff <jmartinbaldwin@gmail.com>
2024-05-01 15:04:26 +0900

    changed the code to match the updated Odin RPS instructions.
    I have the basic functions set up except for the
    play round function.

--- RPS.js
+++ RPS.js
@@ -2,10 +2,10 @@ function rdmNumber(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-let number = rdmNumber(1,3);
-
-
 function getComputerChoice() {
+
+    let number = rdmNumber(1,3);
+
     if (number === 1 ) {
         return "rock"
     } else if (number === 2) {
@@ -15,37 +15,20 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice(){
+function getHumanChoice(){
    return prompt("Please choose Rock, Paper or Scissors").toLowerCase()
 }
 
 
-let computerSelection = getComputerChoice();
+const humanScore = 0
 
-let playerSelection = getPlayerChoice();
+const computerScore = 0 
 
-function playRound(playerSelection, computerSelection){
-    if (playerSelection === "rock" && computerSelection === "rock"){
-        return "rock and rock: it's a tie."
-    } else if (playerSelection === "rock" && computerSelection === "paper"){
-        return "rock and paper: you lose."
-    } else if (playerSelection === "rock" && computerSelection === "scissors"){
-        return "rock and scissors: you win!"
-    } else if (playerSelection === "paper" && computerSelection === "paper"){
-        return "paper and paper: it's a tie."
-    } else if (playerSelection === "paper" && computerSelection === "scissor"){
-        return "paper and scissors: you lose."
-    } else if (playerSelection === "scissors" && computerSelection === "scissors"){
-        return "scissors and scissors: it's a tie."
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "scissors and rock: you lose!"
-    } else if (playerSelection === "scissors" && computerSelection === "paper"){
-        return "scissors and paper: you win!"
-    } else if (playerSelection === "paper" && computerSelection === "rock"){
-        return "paper and rock: you win!"
-    } else {
-        return "You must have typed something incorrectly. Please try again."
-    }
+function playRound(humanChoice, computerChoice){
+
+
 }
 
-console.log(playRound(playerSelection, computerSelection));
+const humanSelection = getHumanChoice();
+
+const computerSelection = getComputerChoice();
